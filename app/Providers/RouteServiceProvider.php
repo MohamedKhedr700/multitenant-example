c45f879472996662bb6731938926d24f4c1d1c30
mohamedkhedr700 <M.khedr@rowaad.net>
2024-01-16 23:12:29 +0200

    Add multitenancy package

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -14,8 +14,6 @@ class RouteServiceProvider extends ServiceProvider
      * The path to your application's "home" route.
      *
      * Typically, users are redirected here after authentication.
-     *
-     * @var string
      */
     public const HOME = '/home';
 
@@ -24,17 +22,77 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureRateLimiting();
+
+        $this->routes(function () {
+            $this->mapTenantApiRoutes();
+            $this->mapTenantWebRoutes();
+            $this->mapApiRoutes();
+            $this->mapWebRoutes();
+        });
+    }
+
+    /**
+     * Configure rate limiting.
+     */
+    protected function configureRateLimiting(): void
+    {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+    }
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+    /**
+     * Map web routes.
+     */
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')
+            ->group(base_path('routes/web.php'));
+    }
 
+    /**
+     * Map api routes.
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+    }
+
+    /**
+     * Map tenant web routes.
+     */
+    protected function mapTenantWebRoutes(): void
+    {
+        foreach ($this->centralDomains() as $domain) {
             Route::middleware('web')
+                ->domain($domain)
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
-        });
+        }
+    }
+
+    /**
+     * Map tenant api routes.
+     */
+    protected function mapTenantApiRoutes(): void
+    {
+        foreach ($this->centralDomains() as $domain) {
+            Route::prefix('api')
+                ->domain($domain)
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
+        }
+    }
+
+    /**
+     * Gent tenancy central domains.
+     */
+    protected function centralDomains(): array
+    {
+        return config('tenancy.central_domains');
     }
 }
---
+++ docker/phpmyadmin/config.user.inc.php
@@ -0,0 +1,3 @@
+<?php
+
+$cfg['LoginCookieValidity'] = (86400 * 30); // 86400 is 24 hours in seconds, this is 30 days.
